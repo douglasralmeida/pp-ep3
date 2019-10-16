0fd210473f42a5e42f5a5d44e6517e1ccb01d8cd
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2019-10-15 23:04:42 -0300

    Testes

---
+++ teste.sh
@@ -0,0 +1,41 @@
+mpiexec -n 2 ./bin/ep3 < entrada4
+mpiexec -n 2 ./bin/ep3 < entrada8
+mpiexec -n 2 ./bin/ep3 < entrada16
+mpiexec -n 2 ./bin/ep3 < entrada32
+mpiexec -n 2 ./bin/ep3 < entrada64
+mpiexec -n 2 ./bin/ep3 < entrada128
+
+mpiexec -n 4 ./bin/ep3 < entrada4
+mpiexec -n 4 ./bin/ep3 < entrada8
+mpiexec -n 4 ./bin/ep3 < entrada16
+mpiexec -n 4 ./bin/ep3 < entrada32
+mpiexec -n 4 ./bin/ep3 < entrada64
+mpiexec -n 4 ./bin/ep3 < entrada128
+
+mpiexec -n 8 ./bin/ep3 < entrada4
+mpiexec -n 8 ./bin/ep3 < entrada8
+mpiexec -n 8 ./bin/ep3 < entrada16
+mpiexec -n 8 ./bin/ep3 < entrada32
+mpiexec -n 8 ./bin/ep3 < entrada64
+mpiexec -n 8 ./bin/ep3 < entrada128
+
+mpiexec -n 16 ./bin/ep3 < entrada4
+mpiexec -n 16 ./bin/ep3 < entrada8
+mpiexec -n 16 ./bin/ep3 < entrada16
+mpiexec -n 16 ./bin/ep3 < entrada32
+mpiexec -n 16 ./bin/ep3 < entrada64
+mpiexec -n 16 ./bin/ep3 < entrada128
+
+mpiexec -n 32 ./bin/ep3 < entrada4
+mpiexec -n 32 ./bin/ep3 < entrada8
+mpiexec -n 32 ./bin/ep3 < entrada16
+mpiexec -n 32 ./bin/ep3 < entrada32
+mpiexec -n 32 ./bin/ep3 < entrada64
+mpiexec -n 32 ./bin/ep3 < entrada128
+
+mpiexec -n 64 ./bin/ep3 < entrada4
+mpiexec -n 64 ./bin/ep3 < entrada8
+mpiexec -n 64 ./bin/ep3 < entrada16
+mpiexec -n 64 ./bin/ep3 < entrada32
+mpiexec -n 64 ./bin/ep3 < entrada64
+mpiexec -n 64 ./bin/ep3 < entrada128
